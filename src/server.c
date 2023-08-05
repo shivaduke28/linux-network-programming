42137dd933127020d8a4228f1be9598eed5de470
shivaduke28 <shivaduke28@gmail.com>
2023-08-06 04:42:39 +0900

    refactor

--- src/server.c
+++ src/server.c
@@ -10,13 +10,13 @@
 
 int main()
 {
-    int len;
-    int sock;
-    int bin;
+    int server_socket, client_socket;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t client_addr_len = sizeof(client_addr);
 
     // socketの生成
-    int sock0 = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock0 == -1)
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket == -1)
     {
         perror("socket");
         printf("%d\n", errno);
@@ -26,61 +26,57 @@ int main()
     // socketの設定
     // ポート番号
     // どのアドレスからの接続も受け入れるように待ち受ける
-    struct sockaddr_in addr;
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(12345);
-    addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(12345);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
 
     // ソケットのオプションを指定
     int reuseaddr = 1;
-    setsockopt(sock0, SOL_SOCKET, SO_REUSEADDR, &reuseaddr, sizeof(reuseaddr));
+    setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &reuseaddr, sizeof(reuseaddr));
 
     // bindする
-    if (bind(sock0, (struct sockaddr *)&addr, sizeof(addr)) == -1)
+    if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1)
     {
         perror("bind");
         printf("%d\n", errno);
-        close(sock0);
+        close(server_socket);
         exit(EXIT_FAILURE);
     }
 
     // TCPクライアントからの接続要求を待てる状態にする
-    if (listen(sock0, 5) == -1)
+    if (listen(server_socket, 5) == -1)
     {
         perror("listen");
-        close(sock0);
+        close(server_socket);
         exit(EXIT_FAILURE);
     }
-
-    struct sockaddr_in client;
 
     while (1)
     {
         // TCPクライアントからの接続要求を受け付ける
-        len = sizeof(client);
-        sock = accept(sock0, (struct sockaddr *)&client, &len);
-        if (sock < 0)
+        client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &client_addr_len);
+        if (client_socket < 0)
         {
             perror("accept");
             printf("%d\n", errno);
             break;
         }
 
-        printf("accepted connection from %s, port=%d\n", inet_ntoa(client.sin_addr), ntohs(client.sin_port));
+        printf("accepted connection from %s, port=%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
 
         // 5文字送信
-        if (write(sock, "HELLO", 5) == -1)
+        if (write(client_socket, "HELLO", 5) == -1)
         {
             perror("write");
             break;
         }
 
         // TCPセッションの終了
-        close(sock);
+        close(client_socket);
     }
 
     // listenするソケットの終了
-    close(sock0);
+    close(server_socket);
 
     exit(EXIT_SUCCESS);
 }
